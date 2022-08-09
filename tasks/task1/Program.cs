46d74ef4f08a064cb27a11dd1415689ca87185ff
kaatarn-protecs <golddez@gmail.com>
2022-08-09 10:31:44 +0300

    Решение первой задачи

--- tasks/task1/Program.cs
+++ tasks/task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] 
+// и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+
+int number = new Random().Next(10, 100); // Отрезок для генератора псевдослучайных чисел
+Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
+int firstDigit = number / 10; // Получаем первую цифру, поделив на 10
+int secondDigit = number % 10; // Получаем вторую цифру, получив остаток от деления на 10
+
+if (firstDigit == secondDigit) Console.WriteLine($"Наибольшая цифра числа {number} -> Цифры одинаковые");
+else if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {number} -> {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа {number} -> {secondDigit}");
