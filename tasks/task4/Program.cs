2bf545419d27b0afc4e2110d787252fcef9166fc
kaatarn-protecs <golddez@gmail.com>
2022-08-09 11:58:55 +0300

    Решение задач 4 и 5

---
+++ tasks/task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Multip(int num, int a, int b)
+{
+    return num % a == 0 && num % b == 0; // Возращает TRUE, если условие работает
+}
+
+if (Multip(number, 7, 23)) Console.WriteLine($"{number} -> Да");
+else Console.WriteLine($"{number} -> Нет");
